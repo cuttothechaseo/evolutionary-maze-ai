12651715e40d33a3abd0de0694a8536a47f8e7ae
Chase Ottimo <chaseottimo@gmail.com>
2026-09-15 07:32:44 -0400

    refactored and simplified maze

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-from generated_maze import create_maze, print_maze
-
+from maze import MAZE, print_maze
 
 if __name__ == "__main__":
     # Generate and print the maze
-    maze_grid = create_maze(width=39, height=19)
-    print_maze(maze_grid)
+    print_maze(MAZE)
---
+++ maze.py
@@ -0,0 +1,35 @@
+# 1 == wall, 0 == open space
+MAZE = [
+    [1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 1],
+    [1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1],
+]
+
+START = (1, 1)  # MAZE [1][1]
+GOAL = (5, 5)
+
+
+def print_maze(maze):
+    for row_index, row in enumerate(maze):
+        for column_index, cell in enumerate(row):
+            position = (row_index, column_index)
+
+            if position == START:
+                print("S", end=" ")
+            elif position == GOAL:
+                print("G", end=" ")
+            elif cell == 1:
+                print("█", end=" ")
+            else:
+                print(" ", end=" ")
+
+        print()
+
+
+if __name__ == "__main__":
+    # Generate and print the maze
+    maze = print_maze(MAZE)
